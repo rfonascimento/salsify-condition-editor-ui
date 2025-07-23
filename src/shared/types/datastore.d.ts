e66b41d4c9f6c8482e4c36ddf5aefa70b0b742e3
Ricardo Nascimento <nar1av@bosch.com>
2025-07-23 11:07:37 +0100

    SALSIFY: Include datastore object in project and add typings for it

---
+++ src/shared/types/datastore.d.ts
@@ -0,0 +1,41 @@
+// datastore.d.ts
+
+export type PropertyValue = {
+  property_id: number;
+  value: string | number;
+};
+
+export type Product = {
+  id: number;
+  property_values: PropertyValue[];
+};
+
+export type PropertyType = 'string' | 'number' | 'enumerated';
+
+export type Property = {
+  id: number;
+  name: string;
+  type: PropertyType;
+  values?: string[]; // only present if type === 'enumerated'
+};
+
+export type Operator = {
+  text: string;
+  id: string;
+};
+
+export type DataStore = {
+  getProducts: () => Product[];
+  getProperties: () => Property[];
+  getOperators: () => Operator[];
+  products: Product[];
+  properties: Property[];
+  operators: Operator[];
+};
+
+// Add datastore to the global Window object
+declare global {
+  interface Window {
+    datastore: DataStore;
+  }
+}
